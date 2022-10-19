2b07d9b5d2048502cb69a5f9f8d02aa637cf3670
Avstriez <alexej.terehin@mail.ru>
2022-10-19 12:45:06 +0300

    finished Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4...4"
+
+Console.WriteLine("Напишите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int count = -number;
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count++;
+    }
+}
+else Console.WriteLine("Введите целое положительное число"); 
