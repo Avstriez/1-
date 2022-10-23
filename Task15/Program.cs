10ef68c0e749ea385402e559d91fede0e8a65492
Avstriez <alexej.terehin@mail.ru>
2022-10-23 21:05:53 +0300

    finished homework15

---
+++ Task15/Program.cs
@@ -0,0 +1,24 @@
+﻿// // Напишите программу, которая принимает на вход цифру,
+// // обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Day of the week ");
+int weekNumber = Convert.ToInt32(Console.ReadLine());
+
+void WeekDayTest(int number)
+{
+    if (weekNumber == 6 || weekNumber == 7)
+    {
+        Console.WriteLine("weekend!");
+    }
+    else if (weekNumber < 1 || weekNumber > 7)
+    {
+        Console.WriteLine("r u ok?");
+    }
+    else Console.WriteLine("Today is not your day!");
+}
+WeekDayTest(weekNumber);
