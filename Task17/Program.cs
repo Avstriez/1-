9d194635e4d269adfcddbb9d3dc1c1e29987eecb
Avstriez <alexej.terehin@mail.ru>
2023-01-25 13:42:39 +0300

    it worksgit add .

---
+++ Task17/Program.cs
@@ -0,0 +1,38 @@
+﻿//Напишите программу, которая принимает на вход координаты точки (X и Y),
+//причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+void CheckQuaters(int x, int y) //создаем второй метов(функцию)
+{
+    if (x > 0 & y > 0)
+    {
+        Console.WriteLine(1);
+    }
+    if (x < 0 & y > 0)
+    {
+        Console.WriteLine(2);
+    }
+    if (x < 0 & y < 0)
+    {
+        Console.WriteLine(3);
+    }
+    if (x > 0 & y < 0)
+    {
+        Console.WriteLine(4);
+    }
+}
+
+
+int GetNum(string text) //создаем первый метод(функцию)
+{
+    Console.WriteLine(text);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+//Ввод данных от пользователя х, у
+int x = GetNum("Введите значение х");
+int y = GetNum("Введите значение y");
+
+//Определить четверть
+CheckQuaters(x, y);
+
