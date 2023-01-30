6a26e3f79dcd25600fe823f0b96fc675f12c00a5
Avstriez <alexej.terehin@mail.ru>
2023-01-30 19:41:26 +0300

    finished task26

---
+++ Task26/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+void LenNumber(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        result++;
+    }
+    Console.Write(result);
+}
+
+
+int GetNumber(string text) //создаем первый метод(функцию)
+{
+    Console.WriteLine(text);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+//1. Ввод данных
+int number = GetNumber("ВВедите число: ");
+
+Console.Write($"{number} -> ");
+
+LenNumber(number);
