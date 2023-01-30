3073f930917e194e5e099498ba5294e97297fbdd
Avstriez <alexej.terehin@mail.ru>
2023-01-30 18:32:49 +0300

    finished task24

---
+++ Task24/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
+
+
+
+int GetSum(int num)
+{
+    int result = 0;
+    for (int i = 1; i <= num; i++)  // так тоже можно for (int i = 1; i <= num; i++) result = result + i;
+    {                               //                return result;
+        result = result + i;
+    }
+    return result;
+}
+int GetNumber(string text) //создаем первый метод(функцию)
+{
+    Console.WriteLine(text);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+//1. Ввод данных
+int number = GetNumber("ВВедите число: ");
+
+
+//2. В цикле суммируем все числа
+
+
+Console.WriteLine(number + " -> " + GetSum(number));
+
