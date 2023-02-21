b7a8b9acdd39298b45cef928df5c7afc971d2b7b
Avstriez <alexej.terehin@mail.ru>
2023-02-21 12:25:40 +0300

    finished task48

---
+++ Task48/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5     */
+
+int GetNum(string text)
+{
+    Console.WriteLine(text);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int[,] GenerateMatrix(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = i + j;
+        }
+    }
+    return result;
+}
+
+void ShowMatrix(int[,] matrix)
+{   
+    int rows = matrix.GetLength(0);
+    int colums = matrix.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            Console.Write(matrix[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetNum("Введите количество строк: ");
+int n = GetNum("Введите количество столбцов: ");
+int[,] matrix = GenerateMatrix(m, n);
+ShowMatrix(matrix);
