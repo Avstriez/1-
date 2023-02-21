3da25f7fa3d9fd6e6bed0f8cabe9a3c497050345
Avstriez <alexej.terehin@mail.ru>
2023-02-21 13:11:06 +0300

    finished task49

---
+++ Task49/Program.cs
@@ -0,0 +1,61 @@
+﻿/*Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+Например, изначально массив
+выглядел вот так:
+1 4 7 2     
+5 9 2 3
+8 4 2 4     */
+
+int GetNum(string text)
+{
+    Console.WriteLine(text);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int[,] GenerateMatrix(int rows, int colums, int from, int to)
+{
+    int[,] result = new int[rows, colums];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            result[i,j] = new Random().Next(from, to + 1);
+        }
+    }
+    return result;
+}
+
+void ShowMatrix(int[,] matrix)
+{   
+    int rows = matrix.GetLength(0);
+    int colums = matrix.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            Console.Write(matrix[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeToSquareEvenIndex(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) arr[i, j] *= arr[i, j];
+            else arr[i, j] = arr[i, j];
+        }
+    }
+    return arr;
+} 
+
+int m = GetNum("Введите количество строк: ");
+int n = GetNum("Введите количество столбцов: ");
+int[,] matrix = GenerateMatrix(m, n, 1, 10);
+ShowMatrix(matrix);
+System.Console.WriteLine("------------------------");
+int[,] chagedArray = ChangeToSquareEvenIndex(matrix);
+ShowMatrix(chagedArray);
