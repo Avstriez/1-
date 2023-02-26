1774b4b4df64ffd0975a7c4235cc710164496b80
Avstriez <alexej.terehin@mail.ru>
2023-02-26 14:58:39 +0300

    finished task57

---
+++ Task57/Program.cs
@@ -0,0 +1,60 @@
+﻿/* (не работает)Составить частотный словарь элементов двумерного массива.
+Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.    */
+
+int GetNum(string text)
+{
+    Console.WriteLine(text);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int[,] GenerateMatrix(int rows, int colums, int from, int to)
+{
+    int[,] result = new int[rows, colums];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            result[i, j] = new Random().Next(from, to + 1);
+        }
+    }
+    return result;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int colums = matrix.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] Element(int[,] matrix)
+{
+    int[] arrayNew = new int[10];
+    for (int i = 0; i < arrayNew.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayNew.GetLength(1); j++)
+        {
+            arrayNew[matrix[i, j]] += 1;
+        }
+    }
+    return arrayNew;
+}
+
+int m = GetNum("Введите количество строк: ");
+int n = GetNum("Введите количество столбцов: ");
+int[,] matrix = GenerateMatrix(m, n, 0, 9);
+ShowMatrix(matrix);
+int[] element = Element(matrix);
+for (int i = 0; i < element.Length; i++)
+{
+    if (element[i] > 0)
+    Console.WriteLine($"{i} встречается {element[i]} раз"); 
+}
